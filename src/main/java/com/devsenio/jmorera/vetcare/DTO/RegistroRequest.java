000754c80565e72a52925bd794e1d4c72335ea98
Ing John Morera Barrera <johnmorera.89@gmail.com>
2026-07-16 21:26:50 -0500

    correcciòn de endpoints

--- src/main/java/com/devsenio/jmorera/vetcare/DTO/RegistroRequest.java
+++ src/main/java/com/devsenio/jmorera/vetcare/DTO/RegistroRequest.java
@@ -3,13 +3,14 @@ package com.devsenio.jmorera.vetcare.DTO;
 import com.devsenio.jmorera.vetcare.model.Rol;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RegistroRequest(
 
-        @NotBlank(message = "El nombre de usuario es obligatorio") String username,
+                @NotBlank(message = "El nombre de usuario es obligatorio") String username,
 
-        @NotBlank(message = "La contraseña es obligatoria") String password,
+                @NotBlank(message = "La contraseña es obligatoria") String password,
 
-        @NotBlank(message = "El rol es obligatorio") Rol rol) {
+                @NotNull(message = "El rol es obligatorio") Rol rol) {
 
 }
--- src/main/java/com/devsenio/jmorera/vetcare/controller/AuthController.java
+++ src/main/java/com/devsenio/jmorera/vetcare/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.devsenio.jmorera.vetcare.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,6 @@ import com.devsenio.jmorera.vetcare.DTO.LoginRequest;
 import com.devsenio.jmorera.vetcare.DTO.RegistroRequest;
 import com.devsenio.jmorera.vetcare.service.AuthService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
